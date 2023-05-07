236b654e01736e331e50f7cbecf4bb6a7e261ab8
Craig Robert Shenton <craig.shenton@nhs.net>
2023-05-07 22:19:12 +0100

    charts

--- open_health_stats/org_github_api_call.py
+++ open_health_stats/org_github_api_call.py
@@ -12,4 +12,4 @@ access_token = "token"
 raw_github_df = github_api_call.pull_raw_df(config["github_org_dict"])
 
 # save file to .csvç
-raw_github_df.to_csv("data/org_repos_raw.csv", index=False)
+raw_github_df.to_csv("open_health_stats/data/org_repos_raw.csv", index=False)
