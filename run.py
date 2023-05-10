efdf8d19079140eec6dcd19bf517ab3b1d0fd00d
Craig Robert Shenton <craig.shenton@nhs.net>
2023-05-10 15:30:13 +0100

    Version 0.2.0 (#6)
    # Major Release 0.2.0
    
    ## Change log
     - Refactored all code inline with [NHSD RAP community python template](https://github.com/NHSDigital/rap-package-template)
     - Fixed run errors with [GitHub API query](https://github.com/nhs-pycom/opensource-health-statistics/blob/version3/src/ingestion/github_api_call.py), made code much more robust
     - Publication now runs on [NHS-R Quarto Theme](https://github.com/nhs-r-community/quarto-nhs-theme) for ease of development

--- run.py
+++ run.py
@@ -1,235 +1,58 @@
-import yaml
+"""
+Python notebook source
+-------------------------------------------------------------------------
+Copyright (c) 2023 NHS Python Community. All rights reserved.
+Licensed under the MIT License. See license.txt in the project root for
+license information.
+-------------------------------------------------------------------------
+
+FILE:           run.py
+DESCRIPTION:    Process GitHub API data
+
+CONTRIBUTORS:   Craig R. Shenton
+CONTACT:        craig.shenton@nhs.net
+CREATED:        10 May 2023
+VERSION:        0.2.0
+
+-------------------------------------------------------------------------
+"""
 import pandas as pd
-from datetime import datetime
-import plotly
-import plotly.graph_objects as go
-import plotly.express as px
+import timeit 
+from pathlib import Path
+import logging
+from src.ingestion.github_api_call import query_org_repos
+from src.processing.data_processing import tidy_raw_df, add_missing_values_and_filter
+from src.utils.file_paths import get_config
+from src.utils.load_yaml import load_yaml
+from src.utils.logging_config import configure_logging
 
-import github as github
-import gitlab as gitlab
+logger = logging.getLogger(__name__)
 
-# Load in the config parameters
-with open("config.yaml", "r") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
+def main():
+    # load config, here we load our project's parameters from the config.toml file
+    config = get_config("config.toml") 
+    raw_sink = Path(config['raw_sink'])
+    agg_sink = Path(config['agg_sink'])
+    org_list = Path(config['org_list'])
+    log_dir = Path(config['log_dir'])
 
-# Data collection
+    # configure logging
+    configure_logging(log_dir, config)
+    logger.info(f"Configured logging with log folder: {log_dir}.")
 
-# Pull the raw data from the APIs
-raw_github_df = github.pull_raw_df(config["github_org_dict"])
-raw_gitlab_df = gitlab.pull_raw_df(config["gitlab_group_dict"])
+    # run data pipeline
+    yaml_file = load_yaml(org_list)
+    raw_github_df = query_org_repos(github_org_dict = yaml_file["github_org_dict"], max_retries=3)
+    raw_github_df.to_csv(raw_sink, index=False)
+    logger.info(f"Saved raw data in folder: {raw_sink}.")
+    #raw_github_df = pd.read_csv(raw_sink) # for testing
+    agg_github_df = add_missing_values_and_filter(tidy_raw_df(raw_github_df))
+    agg_github_df.to_csv(agg_sink, index=False)
+    logger.info(f"Saved aggregate data in folder: {agg_sink}.")
 
-# Tidy the raw data
-tidy_github_df = github.tidy_raw_df(raw_github_df)
-tidy_gitlab_df = gitlab.tidy_raw_df(raw_gitlab_df)
-
-# Combine tidy dataframes
-df = pd.concat([tidy_github_df, tidy_gitlab_df]).reset_index(drop=True)
-
-# Data processing
-
-# Make an org_short hyperlink column and make the org column a hyperlink
-df["org_short"] = (
-    "<a href='"
-    + df["link"]
-    + "'>"
-    + df["org"].apply(lambda x: x[:13] + "..." if len(x) > 16 else x)
-    + "</a>"
-)
-df["org"] = "<a href='" + df["link"] + "'>" + df["org"] + "</a>"
-
-# Now we have a standardised table we can begin to split and aggregate... start
-# by changing date to a date type (day only)
-df["date"] = pd.to_datetime(df["date"]).apply(lambda x: x.strftime("%Y-%m-%d"))
-
-# Cumulative sum by org, link and date of the numerical columns
-aggregate_df = (
-    df.groupby(["org", "org_short", "date"])
-    .sum()
-    .groupby(level=[0])
-    .cumsum()
-    .reset_index()
-)
-
-# Now we need to get the top license + language at each date for each
-# organisation. This is not so straight forward but wrapped in a function as
-# is the same for both columns
-def create_top_column_df(df, column):
-    return (
-        df
-        # Get the count of new columns values at each date
-        .groupby(["org", "date", column])
-        .size()
-        # Convert to a cumulative count of the column values
-        .groupby(level=[0, 2])
-        .cumsum()
-        .reset_index(level=column)
-        # Get a column per value
-        .pivot(columns=column)
-        .droplevel(0, axis=1)
-        # Forward fill so that each column has the previous value until it
-        # increases again
-        .groupby(["org"])
-        .ffill()
-        # Convert to long and remove NaNs
-        .reset_index()
-        .melt(id_vars=["org", "date"], var_name=column, value_name="count")
-        .dropna()
-        # Keep the column value with the largest count each day
-        .sort_values(by=["org", "date", "count"])
-        .drop_duplicates(subset=["org", "date"], keep="last")
-        # Get rid of the count column
-        .drop(columns=["count"])
-    )
-
-
-top_license_df = create_top_column_df(df, "license")
-top_language_df = create_top_column_df(df, "language")
-
-# Now merge these back onto the aggregate_df
-aggregate_df = (
-    aggregate_df
-    # Left join as we will not have a top license + language on a given date if
-    # the column was a NaN or None
-    .merge(top_license_df, how="left")
-    .merge(top_language_df, how="left")
-    # Forward fill so that NaN is the previous value - standdard .ffill()
-    # doesn't work so has to be wrapped in a lambda
-    # https://stackoverflow.com/questions/63272417/pandas-groupby-drops-group-columns-after-fillna-in-1-1-0
-    .groupby(["org"])
-    .apply(lambda df: df.ffill())
-)
-
-# Output data
-
-# Make the columns nice
-aggregate_df = aggregate_df.rename(
-    columns={
-        "org": "Organisation",
-        "org_short": "Org Short",
-        "date": "Date",
-        "open_repos": "Open Repositories",
-        "stargazers": "Stargazers",
-        "forks": "Forks",
-        "open_issues": "Open Issues",
-        "license": "Top License",
-        "language": "Top Language",
-    }
-)
-
-# save file to .csv
-aggregate_df.to_csv("assets/data/openhealthstats.csv", index=False)
-
-# Format the latest output table
-aggregate_latest_df = (
-    aggregate_df.groupby("Organisation")
-    .tail(1)
-    .sort_values("Open Repositories", ascending=False)
-    .drop(columns=["Org Short", "Date"])
-)
-
-# Create output table (NHS.UK version)
-aggregate_latest_df[
-    ["Open Repositories", "Stargazers", "Forks", "Open Issues"]
-] = aggregate_latest_df[
-    ["Open Repositories", "Stargazers", "Forks", "Open Issues"]
-].astype(
-    int
-)
-aggregate_latest_html = aggregate_latest_df.to_html(
-    index=False, render_links=True, escape=False
-)
-aggregate_latest_html = aggregate_latest_html.replace(
-    "dataframe", "nhsuk-table__panel-with-heading-tab"
-)
-aggregate_latest_html = aggregate_latest_html.replace('border="1"', "")
-with open("_includes/NHSUK_table.html", "w") as file:
-    file.write(aggregate_latest_html)
-
-# Add todays date to a version of the latest output table
-aggregate_latest_df_ = aggregate_latest_df.copy()
-aggregate_latest_df_["Date"] = datetime.now().strftime("%Y-%m-%d")
-
-# Add the latest output table as a final row on aggregate_df with todays date
-aggregate_df = pd.concat([aggregate_df, aggregate_latest_df_])
-
-# Use the ordering of the output table to ensure lines get added to the plot
-# in the correct order
-aggregate_df["Organisation"] = pd.Categorical(
-    values=aggregate_df["Organisation"],
-    categories=aggregate_latest_df["Organisation"],
-    ordered=True,
-)
-
-# Initialise plot
-fig = go.Figure()
-
-# Loop over each org and add line to plot
-for (_, org_short), org_df in aggregate_df.groupby(["Organisation", "Org Short"]):
-
-    # Add the trace plot
-    fig.add_trace(
-        go.Scatter(
-            x=org_df["Date"],
-            y=org_df["Open Repositories"],
-            mode="lines",
-            name=org_short,
-            line={"shape": "hvh"},
-        )
-    )
-
-# Make our own colour scale from plotly.express
-colour_scale = px.colors.qualitative.Dark24 + px.colors.qualitative.Light24
-
-# Loop through chart after adding traces to change colours
-num_orgs = len(aggregate_df["Organisation"].unique())
-for i in list(range(num_orgs)):
-    fig["data"][i]["line"]["color"] = colour_scale[i]
-
-# Asthetics of the plot
-fig.update_layout(
-    {
-        "plot_bgcolor": "rgba(240, 244, 245, 1)",
-        "paper_bgcolor": "rgba(240, 244, 245, 1)",
-    },
-    autosize=True,
-    margin=dict(l=50, r=50, b=50, t=50, pad=4, autoexpand=True),
-    height=500,
-    hovermode="x",
-)
-
-# Add title and dynamic range selector to x axis
-fig.update_xaxes(
-    title_text="<b>" + "Date" + "<b>",
-    rangeselector=dict(
-        buttons=list(
-            [
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all"),
-            ]
-        )
-    ),
-)
-
-# Add title to y axis
-fig.update_yaxes(title_text="<b>" + "Open Repositories" + "<b>")
-
-# Write out to file (.html)
-config = {"displayModeBar": False, "displaylogo": False}
-plotly_chart = plotly.offline.plot(
-    fig, include_plotlyjs=False, output_type="div", config=config
-)
-with open("_includes/plotly_chart.html", "w") as file:
-    file.write(plotly_chart)
-
-# Grab timestamp
-data_updated = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-
-# Write out to file (.html)
-html_str = (
-    '<p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zm.5 4.75a.75.75 0 00-1.5 0v3.5a.75.75 0 00.471.696l2.5 1a.75.75 0 00.557-1.392L8.5 7.742V4.75z"></path></svg> Latest Data: '
-    + data_updated
-    + "</p>"
-)
-with open("_includes/update.html", "w") as file:
-    file.write(html_str)
+if __name__ == "__main__":
+    print("Running processing script")
+    start_time = timeit.default_timer()
+    main()
+    total_time = timeit.default_timer() - start_time
+    print(f"Running time of processing script: {int(total_time / 60)} minutes and {round(total_time%60)} seconds.\n")
